1305a119b352c1c4c1b6a9f04af48140628268e3
Madeleine <majak@itu.dk>
2025-11-20 18:06:11 +0100

    new proto files and more

--- Auction/client/client.go
+++ Auction/client/client.go
@@ -7,14 +7,17 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"os/user"
 	"strconv"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-var id int = 0
+var clientId int = 0
+
+var counter int
+
+var client proto.AuctionClient
 
 func main() {
 	conn, err := grpc.NewClient("localhost:5050", grpc.WithTransportCredentials(insecure.NewCredentials()))
@@ -22,37 +25,46 @@ func main() {
 		log.Fatalf("Not working client 1")
 	}
 	log.Println("Client has connected to server")
-	currentUser, err := user.Current()
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
 
-	client := proto.NewAuctionClient(conn)
-	clientId := 0
+	client = proto.NewAuctionClient(conn)
+	clientId = 0
 
-	bid(clientId)
-	go query(clientId)
+	bid()
+	query(clientId)
 }
 
 func query(clientId int) {
 
 	for {
-		fmt.Println("If you want to check the state of the auction, type in check. If you want to bid, type bid.")
+		fmt.Println("If you want to check the state of the auction, type check. If you want to bid, type bid.")
 
 		scanner := bufio.NewScanner(os.Stdin)
 		scanner.Scan()
 		Text := scanner.Text()
 
 		if Text == "check" {
-			send, err := client.Result(context.Background())
+			checkFunc()
 		} else if Text == "bid" {
-			bid(clientId)
+			bid()
 		}
 	}
 
 }
 
-func bid(clientId int) {
+func checkFunc(){
+	check, err := client.Result(context.Background(), &proto.Empty{})
+	if err != nil{
+		log.Println("error while checking state of auction")
+		connect()
+		checkFunc()
+	}
+	log.Println(check.Message)
+}
+
+func bid() {
 	bidAmount := 0
 	fmt.Println("Enter your bid")
 	for {
@@ -63,15 +75,51 @@ func bid(clientId int) {
 		if _, err := strconv.ParseInt(bid, 10, 64); err != nil {
 			fmt.Printf("Please input a valid number")
 		} else {
-			bidAmount = strconv.ParseInt(bid, 10, 64)
+			bidAmount, _ = strconv.Atoi(bid)
 			break
 		}
 	}
-
-	send, err := client.BidIn(context.Background(),
+	log.Println(clientId)
+	send, err := client.Bid(context.Background(),
 		&proto.BidIn{
-			Amount:   bidAmount,
-			ClientId: clientId,
+			Amount:   int64(bidAmount),
+			ClientId: int64(clientId),
 		})
+		if err != nil{
+			log.Println("error while sending client bid")
+			connect()
+			bid()
+		}
+	if clientId == 0{
+		clientId = int(send.BidderId)
+		log.Println(send.BidderId)
+	}
+	log.Println(send.Ack)
+}
 
+func connect(){
+	counter++
+	if counter == 1{
+		conn, err := grpc.NewClient("localhost:5051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+			if err != nil {
+			log.Fatalf("Not working client 1")
+		}
+		log.Println("Client has connected to server")
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+		client = proto.NewAuctionClient(conn)
+		log.Println("i connected to server 5051")
+	}else{
+		conn, err := grpc.NewClient("localhost:5052", grpc.WithTransportCredentials(insecure.NewCredentials()))
+			if err != nil {
+			log.Fatalf("Not working client 1")
+		}
+		log.Println("Client has connected to server")
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+		client = proto.NewAuctionClient(conn)
+		log.Println("i connected to server 5052")
+	}
 }
--- Auction/server/server.go
+++ Auction/server/server.go
@@ -8,6 +8,7 @@ import(
 	"strconv"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 type Auction_service struct{
@@ -20,40 +21,47 @@ type Auction_service struct{
 	bids    map[int]int
 	ports	[]string
 	peers   map[string]proto.AuctionClient //client pointing to other servers
-	
+	counter int
+	listener net.Listener
 }
 
 func main(){
-	server := &Auction_service{
-		bids: make(map[int]int),
-		highest: 0,
-	}
+	
 	ports := []string{
 		":5050",
 		":5051",
 		":5052",
 	} 
 
-	
+	for _, port := range ports{
+		server := &Auction_service{
+			bids: make(map[int]int),
+			highest: 0,
+			counter: 0,
+		}
+		go server.start_server(port,ports)
+	}
 
-	go server.start_server(":5050",ports)
+	/*go server.start_server(":5050",ports)
 	go server.start_server(":5051",ports)
-	go server.start_server(":5052",ports)
+	go server.start_server(":5052",ports)*/
 
 	select{}
 }
 
-func (server *Auction_service) start_server(string numberPort,[]string ports){
+func (server *Auction_service) start_server( numberPort string,  ports []string){
 	server.grpc = grpc.NewServer()
 	listener, err := net.Listen("tcp", numberPort)
-	server.peers = make(map[string]proto.AuctionClient)
-
-
 	if err != nil {
 		log.Fatalf("Did not work 1")
 	}
+	server.peers = make(map[string]proto.AuctionClient)
+	server.listener = listener
+
+	
 
 	log.Println("the server has started")
+	server.ports = ports
 
 	for _, value := range server.ports {
 		if(value != numberPort){
@@ -71,38 +79,46 @@ func (server *Auction_service) start_server(string numberPort,[]string ports){
 	proto.RegisterAuctionServer(server.grpc, server)
 
 	err = server.grpc.Serve(listener)
-
 	if err != nil {
-		log.Fatalf("Did not work 2")
+		log.Println("Did not work 2")
 	}
 
-	server.serverId = serId
-	
 }
 
-func (server *Auction_service) Bid(ctx context.Context, in proto.BidIn) (*proto.BidOut, error){
+func (server *Auction_service) Bid(ctx context.Context, in *proto.BidIn) (*proto.BidOut, error){
+	if server.counter == 1{
+		server.counter = 0
+		log.Printf("shutting down server")
+		server.listener.Close()
+	}
+	
+	server.counter ++
 	amount := in.Amount
 	clientId := in.ClientId
 	if clientId == 0{
 		clientId = int64(len(server.bids)) + 1
+		server.bids[int(clientId)] = 0
 	}
 	
 	if server.highest < int(amount) {
 		server.highest = int(amount)
 		server.bids[int(clientId)] = int(amount)
 	}else{
-		server.bids[int(clientId)] = 0
-		return &proto.BidOut{Ack: "Client " + strconv.FormatInt(clientId, 10) + "Your bid has been rejected", BidderId: clientId}, nil
+		return &proto.BidOut{Ack: "Client " + strconv.FormatInt(clientId, 10) + " Your bid has been rejected", BidderId: clientId}, nil
 	}
 
-	for _, i := range server.peers{
-		
+	for value, i := range server.peers{
+		_, err := i.Update(ctx, in)
+		if err != nil{
+			log.Printf("Could not update the info to server:", value)
+		}
 	}
 
-	return &proto.BidOut{Ack: "Client " + strconv.FormatInt(clientId, 10) + "Your bid has been accepted", BidderId: clientId}, nil
+	return &proto.BidOut{Ack: "Client " + strconv.FormatInt(clientId, 10) + " Your bid has been accepted", BidderId: clientId}, nil
 }
 
-func (server *Auction_service) Update(ctx context.context, in proto.UpdateSend) (*proto.Empty, error){
+func (server *Auction_service) Update(ctx context.Context, in *proto.BidIn) (*proto.Empty, error){
+	
 	amount := in.Amount
 	clientId := in.ClientId
 
@@ -112,7 +128,7 @@ func (server *Auction_service) Update(ctx context.context, in proto.UpdateSend) 
 	return &proto.Empty{}, nil
 }
 
-func (server *Auction_service) Result(ctx context.Context, in proto.Empty) (*proto.ResultSend, error){
+func (server *Auction_service) Result(ctx context.Context, in *proto.Empty) (*proto.ResultSend, error){
 	for client, price := range server.bids {
 		if price == server.highest{
 			return &proto.ResultSend{Message: ("the highest bidder is client: " + strconv.FormatInt(int64(client), 10) + 
